0630a5f82c96e4885ac05a0e18a86601e118933a
Eswar Kambhampati <eswark@Eswars-MacBook-Pro.local>
2023-07-30 20:41:40 +0530

    Partial working commmit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
+import {  List, Button,Space ,Table} from 'antd';
+import React, { useState } from 'react';
+import {BOY_NAMES,GIRL_NAMES} from '../src/constants'
 import './App.css';
 
-function App() {
+const ALPHABET = [
+  'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+  'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
+];
+
+function App(props) {
+  const {setFirstNameFilter} = props;
+
+  const [name,SetName]=useState("");
+
+  const [selected, setSelected] = useState(null);
+
+  const handleClick = char => {
+    if (char === selected) {
+      char = null;
+    }
+    setSelected(char);
+    //setFirstNameFilter(char);
+  }
+
+  const pickRandomName = names => {
+    return names[Math.floor(Math.random() * names.length)]
+
+}
+
+const pickRandomLetter = mapping =>{
+  const randomLetterIndex = Math.floor(Math.random() * Object.keys(mapping).length);
+        return Object.keys(mapping)[randomLetterIndex];
+}
+
+const handleChange = () =>{
+  console.log(pickRandomName(BOY_NAMES[pickRandomLetter(BOY_NAMES)]))
+  SetName(pickRandomName(BOY_NAMES[pickRandomLetter(BOY_NAMES)])) 
+  
+}
+console.log(name)
+const columns = [
+  {
+    title: 'Name',
+    dataIndex: 'name',
+    key: 'name',
+    render: (text) => <a>{text}</a>,
+  }]
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <section className="alphabetContainer">
+      <Space>
+        <Button onClick={handleChange}>Generate Random Name</Button>
+      </Space>
+      {/* <Table columns={columns} dataSource={name}/> */}
+      <List
+        grid={{column: 6}}
+        dataSource={ALPHABET}
+        bordered={true}
+        renderItem={letter => (
+          <List.Item
+            key={letter}
+          >
+            <Button
+              type={selected === letter ? 'primary' : 'default'}
+              size="large"
+              onClick={() => handleClick(letter)}
+              >
+                {letter}
+            </Button>
+            
+          </List.Item>
+        )}
+      />
+    </section>
+    )
+
 }
 
 export default App;
